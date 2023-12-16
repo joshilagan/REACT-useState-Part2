6bee44bf0a31bbac997e2af3ef76fac2d049a50d
joshi <joseph.ilagan.99@gmail.com>
2023-12-16 10:47:23 +0800

    .

--- src/App.js
+++ src/App.js
@@ -6,13 +6,26 @@ function App() {
       <div className='groceryCard'>
         <h1>My Grocery list</h1>
         <ul>
-          <li>16 oz 90% lean ground beef, Lean</li>
-          <li>1/2 cup Onion</li>
-          <li>1 Egg, Large</li>
-          <li>1/2 cup Catsup</li>
-          <li>1/2 tsp Pepper, Ground</li>
-          <li>1/2 tsp Salt</li>
+          <div className='liBlock'>
+            <li>16 oz 90% lean ground beef, Lean  </li> <input type="text" className='listInput' />
+          </div>
+          <div className='liBlock'>
+            <li>1/2 cup Onion</li><input type="text" className='listInput' />
+          </div>
+          <div className='liBlock'>
+            <li>1 Egg, Large</li><input type="text" className='listInput' />
+          </div>
+          <div className='liBlock'>
+            <li>1/2 cup Catsup</li><input type="text" className='listInput' />
+          </div>
+          <div className='liBlock'>
+          <li>1/2 tsp Pepper, Ground</li><input type="text" className='listInput' />
+          </div>
+          <div className='liBlock'>
+            <li>1/2 tsp Salt</li><input type="text" className='listInput' />
+          </div>
         </ul>
+        <button>Compute</button>
         <p>Total Price: </p>
       </div>
     </div>
