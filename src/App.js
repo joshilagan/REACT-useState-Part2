32bd8f29f2b37e27953900e3f7c8dafc4ab4484e
joshi <joseph.ilagan.99@gmail.com>
2023-12-16 10:33:49 +0800

    .

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className='groceryCard'>
         <h1>My Grocery list</h1>
-        <p>16 oz 90% lean ground beef, Lean</p>
-        <p>1/2 cup Onion</p>
-        <p>1 Egg, Large</p>
-        <p>1/2 cup Catsup</p>
-        <p>1/2 tsp Pepper, Ground</p>
-        <p>1/2 tsp Salt</p>
+        <ul>
+          <li>16 oz 90% lean ground beef, Lean</li>
+          <li>1/2 cup Onion</li>
+          <li>1 Egg, Large</li>
+          <li>1/2 cup Catsup</li>
+          <li>1/2 tsp Pepper, Ground</li>
+          <li>1/2 tsp Salt</li>
+        </ul>
+        <p>Total Price: </p>
       </div>
     </div>
   );
