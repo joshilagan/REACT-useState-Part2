06fe17adfe08a06315fddcd2621243f82ef066a7
joshi <joseph.ilagan.99@gmail.com>
2023-12-16 10:26:11 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div>
-        
+        <h1>My Grocery list</h1>
+        <p>16 oz 90% lean ground beef, Lean</p>
+        <p>1/2 cup Onion</p>
+        <p>1 Egg, Large</p>
+        <p>1/2 cup Catsup</p>
+        <p>1/2 tsp Pepper, Ground</p>
+        <p>1/2 tsp Salt</p>
       </div>
     </div>
   );
