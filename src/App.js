d841d7c3fcced267a9eb64e32a4ae384a4819d44
joshi <joseph.ilagan.99@gmail.com>
2023-12-17 12:17:09 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,33 +1,64 @@
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+    const [item1, setItem1] = useState(0);
+    const [item2, setItem2] = useState(0);
+    const [item3, setItem3] = useState(0);
+    const [item4, setItem4] = useState(0);
+    const [item5, setItem5] = useState(0);
+    const [item6, setItem6] = useState(0);
+    
+
+    const handleItem1Change = (item) => {
+      setItem1(item.target.value)
+    }
+    const handleItem2Change = (item) => {
+      setItem2(item.target.value)
+    }
+    const handleItem3Change = (item) => {
+      setItem3(item.target.value)
+    }
+    const handleItem4Change = (item) => {
+      setItem4(item.target.value)
+    }
+    const handleItem5Change = (item) => {
+      setItem5(item.target.value)
+    }
+    const handleItem6Change = (item) => {
+      setItem6(item.target.value)
+    }
+
+    const total = item1 + item2 + item3 + item4 + item5 +item6;
+
   return (
     <div className="App">
       <div className='groceryCard'>
         <h1>My Grocery list</h1>
         <ul>
           <div className='liBlock'>
-            <li>16 oz 90% lean ground beef, Lean  </li> <input type="text" className='listInput' />
+            <li>16 oz 90% lean ground beef, Lean  </li> <input type="text" className='listInput' onChange={handleItem1Change}/>
           </div>
           <div className='liBlock'>
-            <li>1/2 cup Onion</li><input type="text" className='listInput' />
+            <li>1/2 cup Onion</li><input type="text" className='listInput' onChange={handleItem2Change}/>
           </div>
           <div className='liBlock'>
-            <li>1 Egg, Large</li><input type="text" className='listInput' />
+            <li>1 Egg, Large</li><input type="text" className='listInput' onChange={handleItem3Change} />
           </div>
           <div className='liBlock'>
-            <li>1/2 cup Catsup</li><input type="text" className='listInput' />
+            <li>1/2 cup Catsup</li><input type="text" className='listInput' onChange={handleItem4Change}/>
           </div>
           <div className='liBlock'>
-          <li>1/2 tsp Pepper, Ground</li><input type="text" className='listInput' />
+          <li>1/2 tsp Pepper, Ground</li><input type="text" className='listInput' onChange={handleItem5Change}/>
           </div>
           <div className='liBlock'>
-            <li>1/2 tsp Salt</li><input type="text" className='listInput' />
+            <li>1/2 tsp Salt</li><input type="text" className='listInput' onChange={handleItem6Change}/>
           </div>
         </ul>
         <button>Compute</button>
         <div className='priceBlock'>
-          <p>Total Price: </p> <p className='total'></p>
+          <p>Total Price: </p> <p className='total'>{total}</p>
         </div>
       </div>
     </div>
